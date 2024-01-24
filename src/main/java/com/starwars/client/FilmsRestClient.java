a988118ab52922c714a610cc0e4a9f353fb1f5e9
Daniel Kobashigawa (CTW) <Daniel.Kobashigawa@ctw.bmwgroup.com>
2024-01-24 20:42:49 +0000

    Integrated with endpoint to get film by id

--- src/main/java/com/starwars/client/FilmsRestClient.java
+++ src/main/java/com/starwars/client/FilmsRestClient.java
@@ -1,13 +1,18 @@
 package com.starwars.client;
 
+import com.starwars.dto.Film;
 import com.starwars.dto.FilmWrapper;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "filmsRestClient", url = "https://swapi.dev/api/films")
 public interface FilmsRestClient {
 
     @GetMapping
     FilmWrapper getAll();
+
+    @GetMapping("/{id}")
+    Film getById(@PathVariable final Integer id);
 
 }
